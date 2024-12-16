64d919c0167049516fde2f9d9f940f8cc447bb24
Matt Carroll <mcarroll1220@gmail.com>
2024-12-16 18:37:03 -0500

    update launch script

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ set -e  # Exit immediately if a command exits with a non-zero status
 # Run Alembic migrations
 alembic upgrade head
 
-# Start FastAPI
-uvicorn main:app --host 0.0.0.0 --port 8010
+# final two flags are to allow SqlAdmin to work properly
+uvicorn main:app --host 0.0.0.0 --port 8010 --forwarded-allow-ips="*" --proxy-headers
